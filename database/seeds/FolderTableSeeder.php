c28fdedd421886e681eedb0471d67274c0992bbe
dhane31 <dhanegm731@gmail.com>
2019-08-05 11:31:32 +0900

    Add user_id to folders

--- database/seeds/FolderTableSeeder.php
+++ database/seeds/FolderTableSeeder.php
@@ -12,14 +12,16 @@ class FolderTableSeeder extends Seeder
      */
     public function run()
     {
-        $titles = ['プライベート', '仕事', '旅行'];
+      $user = DB::table('users')->first();
+      $titles = ['プライベート', '仕事', '旅行'];
 
-        foreach($titles as $title) {
-          DB::table('folders')->insert([
-            'title' => $title,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-          ]);
-        }
+      foreach($titles as $title) {
+        DB::table('folders')->insert([
+          'title' => $title,
+          'user_id' => $user->id,
+          'created_at' => Carbon::now(),
+          'updated_at' => Carbon::now(),
+        ]);
+      }
     }
 }
